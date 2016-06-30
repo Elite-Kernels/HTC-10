7adf7477663428c062caea4c233fa0ad85d2b618
Steve Pfetsch <spfetsch@google.com>
2016-06-30 15:44:29 -0700

    Revert "Revert "sched: warn/panic upon excessive scheduling latency""
    This reverts commit 556f086bc519ea30239e2e96a1156ae017130a29.
    
    Change-Id: I5483c7c4ece0596198b5a0de1437449d3575e039
    
    Conflicts:
    	include/linux/sched/sysctl.h
    	kernel/sched/fair.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -708,6 +708,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 	},
+#ifdef CONFIG_SCHED_QHMP
 	{
 		.procname	= "sched_max_latency_us",
 		.mode		= 0644,
